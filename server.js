0bb63c9f70e8323614ec4455a88023a9085184f6
Anesu shangwa <97034450+Anesushangwa@users.noreply.github.com>
2022-09-14 15:01:45 +0200

    almost

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 // const hostname = '127.0.0.1';
 const app = express()
-const port = 300
+const port = process.env.port || 300
 
 app.use('/', require('./routes'))
 
