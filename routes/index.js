26d6f84c45c947aeac81abdaf468e1d530c6900e
Anesu shangwa <97034450+Anesushangwa@users.noreply.github.com>
2022-09-19 14:22:46 +0200

    new

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
-const routes = require('express').Router();
+const express = require('express');
+const router = express.Router();
 
-routes.get('/',(req,res) => {
-    res.send('hazel shangwa')
-});
+router.use('/contact', require('./contact'))
 
-module.exports = routes;
+module.exports = router;
