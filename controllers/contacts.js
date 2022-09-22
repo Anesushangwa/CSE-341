37525fe97e5a271137d8ef8cf3a78750e9f639c2
Anesu shangwa <97034450+Anesushangwa@users.noreply.github.com>
2022-09-22 04:13:10 +0200

    crude

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -23,5 +23,79 @@ const getSingle = async (req, res, next) => {
   });
 };
 
-module.exports = { getAll, getSingle };
 
+
+const newinfo = async (req, res) => {
+  const newcontact = {
+    
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      emai: body.email,
+      favcolor: body.favcolor,
+    
+      birthday: req.body.birthday
+  };
+  const sendinfo = await mongodb.getDb().db('anesu').collection('contacts').insertOne(newcontact);
+
+//   try {
+    
+//       res.status(201).json(sendinfo)
+//   }
+//   catch (error) {
+//       res.status(500).json({message: error.message})
+//   };
+// }
+if (sendinfo.acknowledged) {
+  res.status(201).json(sendinfo);
+} else {
+  res.status(500).json(sendinfo.error || 'Some error occurred while creating the contact.');
+}
+};
+
+const updateContact = async (req, res) => {
+const userId = new ObjectId(req.params.id);
+const newcontact= {
+  
+  firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      favcolor: req.body.favcolor,
+      
+      birthday: req.body.birthday
+  
+};
+
+// try {
+//   const dataToSave = await data.save();
+//   res.status(200).json(dataToSave)
+// }
+// catch (error) {
+//   res.status(400).json({message: error.message})
+// }
+// });
+const sendinfo = await mongodb
+.getDb()
+.db('anesu')
+.collection('contacts')
+.replaceOne({ _id: userId }, newcontact);
+console.log(sendinfo);
+if ( sendinfo.modifiedCount > 0) {
+res.status(204).send();
+} else {
+res.status(500).json(sendinfo.error || 'Some error occurred while updating the contact.');
+}
+};
+
+const deleteinfo = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const del = await mongodb.getDb().db('anesu').collection('contacts').remove({ _id: userId }, true);
+  console.log(del);
+  if (del.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(del.error || 'Some error occurred while deleting the contact.');
+  }
+};
+
+
+  module.exports = { getAll, getSingle, newinfo,updateContact,deleteinfo};
--- routes/contacts.js
+++ routes/contacts.js
@@ -7,4 +7,12 @@ router.get('/', contactsController.getAll);
 
 router.get('/:id', contactsController.getSingle);
 
+//Post Method
+router.post('/', contactsController.newinfo)
+
+router.put('/:id', contactsController.updateContact);
+
+router.delete('/:id', contactsController.deleteinfo);
+
+
 module.exports = router;
